59ceb6bfda5b19a0d32fdb8ded9c608321f8612c
Yutaro Morita <morita@YutaronoMacBook-Air.local>
2023-08-02 20:15:16 +0900

    main.jsに加筆しました

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 //HTMLの見た目を作る(innerHTML)
-//ランダム関数で配列をそれぞれランダムに取得
-//配列の要素を取得する共通の関数
-function ranNum(arrayLength) {
-  const num = Math.floor(Math.random * arrayLength);
+//ランダム関数で要素をそれぞれランダムに取得
+function getResult(){
+  const resFortune = fortune[ranNum(fortune)];
+  const resPicture = picture[ranNum(picture)];
+  const resMessage = message[ranNum(message)];
+  const resLuckyLanguage = luckyLanguage[ranNum(luckyLanguage)];
+  const resLuckyDB = luckyDB[ranNum(luckyDB)];
+  const resLuckyEditor = luckyEditor[ranNum(luckyEditor)];
+
+  const result = [resFortune, resPicture, resMessage, resLuckyLanguage, resLuckyDB, resLuckyEditor];
+  return result;
 }
 
-//空の配列を作ってランダムに取得した要素を配列に格納する(res)
-//格納した配列を変数に代入してinnerHTMLに入れる
+//配列の要素を取得する共通の関数
+function ranNum(array) {
+  const num = Math.floor(Math.random * array.length);
+  return num;
+}
+

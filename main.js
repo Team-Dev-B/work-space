25f761a6aa51e91aaa874e46cb6831504fc7ddf6
udonrm <b201395@hiroshima-u.ac.jp>
2023-08-03 10:26:10 +0900

    main.jsの調整

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ function buttonClick() {
   //トップページを非表示にする
   topPage.classList.remove("d-block");
   topPage.classList.add("d-none");
+  let result = getResult();
+  console.log(result);
 
   resultPage.innerHTML = `
   <div class="container">
@@ -15,8 +17,8 @@ function buttonClick() {
         <div class="card" style="width: 18rem">
           <div class="card-body">
             <h5 class="border-bottom">ソフトウェア開発みくじ</h5>
-            <h1>${result[1]}</h1>
-            <p class="border-bottom">${result[0]}</p>
+            <img src = "${result[1]}">
+            <h1 class="border-bottom">${result[0]}</h1>
             <p class="border-bottom">${result[2]}</p>
             <p class="border-bottom">ラッキー言語<br/>${result[3]}</p>
             <p class="border-bottom">ラッキーデータベース<br/>${result[4]}</p>
@@ -41,21 +43,29 @@ let button = document.getElementById("btn");
 button.addEventListener("click", buttonClick);
 
 //ランダム関数で要素をそれぞれランダムに取得
-function getResult(){
+function getResult() {
   const resFortune = fortune[ranNum(fortune)];
-  const resPicture = picture[ranNum(picture)];
+  const resPicture = picture[resFortune];
   const resMessage = message[ranNum(message)];
   const resLuckyLanguage = luckyLanguage[ranNum(luckyLanguage)];
   const resLuckyDB = luckyDB[ranNum(luckyDB)];
   const resLuckyEditor = luckyEditor[ranNum(luckyEditor)];
+  console.log("getResult");
 
-  const result = [resFortune, resPicture, resMessage, resLuckyLanguage, resLuckyDB, resLuckyEditor];
+  const result = [
+    resFortune,
+    resPicture,
+    resMessage,
+    resLuckyLanguage,
+    resLuckyDB,
+    resLuckyEditor,
+  ];
   return result;
 }
 
 //配列の要素を取得する共通の関数
 function ranNum(array) {
-  const num = Math.floor(Math.random * array.length);
+  const num = Math.floor(Math.random() * array.length);
+  console.log("ranNum" + num);
   return num;
 }
-
